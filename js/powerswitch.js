c7b1aa2f1047cf1f10806523463d19f5efbae30d
Ushnish Tapaswi <126304736+plushexe351@users.noreply.github.com>
2023-09-04 02:48:48 +0530

    Added a small instruction at the top of the page for ease of use

--- js/powerswitch.js
+++ js/powerswitch.js
@@ -1,14 +1,19 @@
 const toggleSwitch = document.querySelector('.power-btn');
 const ipod = document.querySelector('.ipod');
 const iPodScreen = document.querySelector('.screen');
+const showinstructions = document.querySelector('.instruction');
+
 
 toggleSwitch.onclick = () => {
 
-    iPodScreen.classList.toggle('active')
+
+    iPodScreen.classList.toggle('active');
 
     toggleSwitch.classList.toggle('active');
 
     setTimeout(() => {
+
+        showinstructions.classList.toggle('active');
 
         ipod.classList.toggle('active');
 
